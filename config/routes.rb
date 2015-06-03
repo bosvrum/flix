b334754817ef00d324857e3076d122a3fd70fc7a
Catalin <catalinboruga@yahoo.com>
2015-06-03 18:39:00 +0200

    Add all the actions to movie controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  
-  get 'movies/index'
 
-  get 'movies' => 'movies#index'
-  # root 'welcome#index'
+  root 'movies#index'
+  
+  # get 'movies' => 'movies#index'
+  # get "movies/:id" => "movies#show", as: "movie"
+  # get 'movies/:id/edit' => 'movies#edit',as: 'edit_movie'
+  # patch "movies/:id" => "movies#update"
+
+  resources :movies
 
 end
