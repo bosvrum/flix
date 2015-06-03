ccbc1b5ab8e6e02079648df7377805c90ea7c942
Catalin <catalinboruga@yahoo.com>
2015-06-03 20:47:14 +0200

    Order the movies by release date

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,7 +3,9 @@ module MoviesHelper
   def format_total_gross(movie)
     if movie.flop?
       content_tag(:strong, 'Flop')
-    else 
+    elsif movie.hit?
+      content_tag(:strong, 'Hit')
+    else  
       number_to_currency(movie.total_gross) 
     end
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,16 @@ class Movie < ActiveRecord::Base
   def flop?
     total_gross.blank? || total_gross < 50000000 
   end
+
+  def hit?
+    total_gross > 300000000
+  end
+
+  def self.released
+    where("released_on <= ?", Time.now).order("released_on DESC")
+  end
+
+  def self.recently_added
+    where("created_at <= ?", Time.now).order("created_at DESC")
+  end
 end
